105eab65e4637aebe72663efa731c7bb8f7e0fc6
simachri <marvin.christe@posteo.de>
2021-08-01 11:49:48 +0200

    feat(DB): Add PostgresDB - endpoint /db/health

--- cmd/djapi/main.go
+++ cmd/djapi/main.go
@@ -1,11 +1,42 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"os"
+
 	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v4"
+	"github.com/joho/godotenv"
 )
 
 func dbHealthCheck(c *gin.Context) {
+	/* The database connection parameters will be loaded from environment variables.
+	 * user=<PGUSER> host=<PGHOST> password=<PGPASSWORD> port=<PGPORT>
+	 * dbname=<PGDATABASE>
+	 * The mapping of environment variables to keyboard is as follows:
+	 * hostaddr -> PGHOST
+	 * port -> PGPORT
+	 * user -> PGUSER
+	 * password -> PGPASSWORD
+	 * dbname -> PGDATABASE
+	 * See `go doc pgconn.ParseConfig` for details.
+	 */
+	conn, err := pgx.Connect(context.Background(), "")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		os.Exit(1)
+	}
+	defer conn.Close(context.Background())
 
+	var greeting string
+	err = conn.QueryRow(context.Background(), "select 'Hello, world!'").Scan(&greeting)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
+		os.Exit(1)
+	}
+
+	c.String(200, fmt.Sprintln(greeting)+"Database connection up and running.")
 }
 
 // https://github.com/gin-gonic/gin#testing
@@ -19,6 +50,8 @@ func setupRouter() *gin.Engine {
 }
 
 func main() {
+	// Spin up environment variables from .env file.
+	godotenv.Load(".env")
 	r := setupRouter()
 	r.Run(":8080")
 }
--- go.mod
+++ go.mod
@@ -8,6 +8,8 @@ require (
 	github.com/go-playground/validator/v10 v10.7.0 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/google/go-cmp v0.5.6 // indirect
+	github.com/jackc/pgx/v4 v4.13.0 // indirect
+	github.com/joho/godotenv v1.3.0
 	github.com/json-iterator/go v1.1.11 // indirect
 	github.com/kisielk/errcheck v1.6.0 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
