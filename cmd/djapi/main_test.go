3084e05605e45a64c4bbb7c5503ff5e7af7e922d
simachri <marvin.christe@posteo.de>
2021-08-04 05:35:28 +0200

    refactor(tests): Use table-driven test setup.

--- cmd/djapi/main_test.go
+++ cmd/djapi/main_test.go
@@ -6,29 +6,36 @@ import (
 	"testing"
 
 	"github.com/joho/godotenv"
-	"github.com/stretchr/testify/assert"
 )
 
-func TestPingRoute(t *testing.T) {
-	router := setupRouter()
+func TestApiRoutes(t *testing.T) {
+	cases := []struct {
+		descr     string
+		httpType  string
+		endpoint  string
+		expCode   int
+		expString string
+	}{
+		{"GET endpoint /ping", "GET", "/ping", 200, "pong"},
+		{"GET endpoint /db/health", "GET", "/db/heatlh", 200, ""},
+	}
 
-	w := httptest.NewRecorder()
-	req, _ := http.NewRequest("GET", "/ping", nil)
-	router.ServeHTTP(w, req)
-
-	assert.Equal(t, 200, w.Code)
-	assert.Equal(t, "pong", w.Body.String())
-}
-
-func TestDbHealthRoute(t *testing.T) {
 	// A DB connection requires environment variables.
 	godotenv.Load("../../.env")
-
 	router := setupRouter()
+	for _, tc := range cases {
+		t.Run(tc.descr, func(t *testing.T) {
 
-	w := httptest.NewRecorder()
-	req, _ := http.NewRequest("GET", "/db/health", nil)
-	router.ServeHTTP(w, req)
+			w := httptest.NewRecorder()
+			req, _ := http.NewRequest("GET", "/ping", nil)
+			router.ServeHTTP(w, req)
 
-	assert.Equal(t, 200, w.Code)
+			if w.Code != tc.expCode {
+				t.Fatalf("expected return code %v, but got %v", tc.expCode, w.Code)
+			}
+			if tc.expString != "" && w.Body.String() != tc.expString {
+				t.Fatalf("expected return string %v, but got %v", tc.expString, w.Body.String())
+			}
+		})
+	}
 }
